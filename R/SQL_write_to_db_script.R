035e4e07b91ac5586194dbb6302e369dc9c7d7e7
briandevoe <bdevoe@brandeis.edu>
2024-04-25 00:37:58 -0400

    add functions and documentation

--- R/SQL_write_to_db_script.R
+++ R/SQL_write_to_db_script.R
@@ -8,17 +8,17 @@
 
 
 
-path <- "C:/Users/bdevoe/Desktop/SQL/"
-files2 <- c("METROS", "NATION", "NATION-METROS", "STATE", "OPP_GAP", "ZIP", "TRACT")
-for(file2 in files2){
-  files <- list.files(paste0(path, file2, "/"))
-  for(file in files){
-    print(file)
-    file_name <- gsub("-", "_", file)
-    infile <- paste0(path, file2, "/", file, "/", file, ".csv")
-    SQL_write(infile = infile, table_name = file_name, database = "coi_test")
-  }
-}
-rm(path,file,files,file2,files2,file_name,infile)
+# path <- "C:/Users/bdevoe/Desktop/SQL/"
+# files2 <- c("METROS", "NATION", "NATION-METROS", "STATE", "OPP_GAP", "ZIP", "TRACT")
+# for(file2 in files2){
+#   files <- list.files(paste0(path, file2, "/"))
+#   for(file in files){
+#     print(file)
+#     file_name <- gsub("-", "_", file)
+#     infile <- paste0(path, file2, "/", file, "/", file, ".csv")
+#     SQL_write(infile = infile, table_name = file_name, database = "coi_test")
+#   }
+# }
+# rm(path,file,files,file2,files2,file_name,infile)
 
 
