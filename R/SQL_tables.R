6a9d8e2c557bd1b2e3f0c4990dcc4047ad345c02
briandevoe <bdevoe@brandeis.edu>
2024-05-08 18:59:57 -0400

    update SQL_tables function - bd

--- R/SQL_tables.R
+++ R/SQL_tables.R
@@ -3,7 +3,8 @@
 #' @author brian devoe
 #'
 #' @description
-#' list available tables and metadata in SQL database
+#' Returns a table that lists all the given tables in a database. Each row contains the table name
+#' and metadata for that table.
 #'
 #' @param database Name of database to connect to. Default is 'coi'.
 
@@ -29,10 +30,26 @@ SQL_tables <- function(database = NULL){
   # for loop to get all tables metadata
   list_tables <- data.table()
   for(table in tables){
+
+    # table name for the actual data table (not metadata or dictionary)
+    table2 <- gsub("_metadata", "", table)
+
+    # load metadata
     dt <- RMariaDB::dbGetQuery(con, paste0("SELECT * FROM ", table, ";"))
     dt$type <- NULL
     dt <- tidyr::pivot_wider(dt, names_from = name, values_from = value)
-    dt <- cbind(table_name = table, dt)
+
+    # column for TRUE/FALSE if dictionary exists
+    dict_exists <- paste0(table2, "_dictionary") %in% tables
+
+    # column count
+    col_count <- RMariaDB::dbGetQuery(con, paste0("SELECT COUNT(*) FROM information_schema.columns WHERE table_name = '", table2, "';"))
+
+    # row count
+    row_count <- RMariaDB::dbGetQuery(con, paste0("SELECT COUNT(*) FROM ", table2, ";"))
+
+    # row and column bind data
+    dt <- cbind(table_name = table, dict_exists = dict_exists, dt, row_count = row_count$`COUNT(*)`, col_count = col_count$`COUNT(*)`)
     list_tables <- rbind(list_tables, dt)
   }
 
