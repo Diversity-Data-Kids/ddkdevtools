9ef5be2bee48adbec7f6946a0944fd2bafb83183
briandevoe <105237070+briandevoe@users.noreply.github.com>
2024-05-14 00:26:01 -0400

    Update SQL_write.R

--- R/SQL_write.R
+++ R/SQL_write.R
@@ -14,27 +14,6 @@ SQL_write <- function(table = NULL, table_name = NULL, database = NULL){
   # start time
   start <- Sys.time()
 
-  # # column names
-  # names <- colnames(table)
-  # for(i in 1:length(names)){
-  #   if(names[i] == "group"){names[i] <- "`group`"}
-  #   class <- class(table[[i]])
-  #   if(class == "character"){
-  #     names[i] <- paste0(names[i], " text(30)")
-  #   } else if(class == "integer"){
-  #     names[i] <- paste0(names[i], " int(30)")
-  #   } else if(class == "numeric"){
-  #     names[i] <- paste0(names[i], " double(30,30)")
-  #   }
-  # }
-  # # fix column names for sql query
-  # names_str <- ""
-  # for(i in 1:length(names)){
-  #   if(i == length(names)){names_str <- paste0(names_str, names[i])}
-  #   else {names_str <- paste0(names_str, names[i], ", ")}
-  # }; rm(i,names,class)
-
-
   # connect to SQL server
   con <- RMariaDB::dbConnect(RMariaDB::MariaDB(),host='129.64.58.140', port=3306,user='dba1', password='Password123$')
 
@@ -50,5 +29,5 @@ SQL_write <- function(table = NULL, table_name = NULL, database = NULL){
   # return time to write
   end <- Sys.time()
   return(paste0("Time to write table: ", end-start))
-
+  
 }
