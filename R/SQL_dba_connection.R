14bceb1352ddddf22a1909459830dadadb4c4225
briandevoe <bdevoe@brandeis.edu>
2024-05-16 18:29:18 -0400

    add dba connection script - bd

---
+++ R/SQL_dba_connection.R
@@ -0,0 +1,21 @@
+#' @author brian devoe
+#' @description
+#' This script is used to activate a connection to the SQL server as the database administrator.
+
+# load database administrator credentials
+source(paste0(dirname(getwd()), "/Credentials/SQL_dba_creds.R"))
+
+# connect to SQL server
+con <- RMariaDB::dbConnect(RMariaDB::MariaDB(),host=host, port=port,user=user, password=password)
+
+# remove credentials
+rm(host, port, user, password, dir)
+
+# SQL query
+dbGetQuery(con, "SHOW databases;")
+dbGetQuery(con, "SHOW tables in ACS;")
+dbGetQuery(con, "SHOW tables in DDK;")
+
+# SQL disconnect
+dbDisconnect(con); rm(con)
+
