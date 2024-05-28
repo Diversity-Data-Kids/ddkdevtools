1eb2b894313369cc48d2a8fe2aede1b391f506c8
briandevoe <bdevoe@brandeis.edu>
2024-05-28 13:39:46 -0400

    add bulk write - bd

--- R/SQL_write_bulk.R
+++ R/SQL_write_bulk.R
@@ -65,7 +65,7 @@ SQL_write_bulk <- function(infile = NULL, table_id = NULL, database = "DDK"){
 
   # write table
   start <- Sys.time()
-  query <- paste0("LOAD DATA LOCAL INFILE '", infile, "' INTO TABLE ", table_id," FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;")
+  query <- paste0("LOAD DATA LOCAL INFILE '", infile, "' INTO TABLE ", table_id," FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n';")
   RMariaDB::dbGetQuery(con, query)
   end   <- Sys.time()
 
