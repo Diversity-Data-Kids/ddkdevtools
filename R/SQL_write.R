70200cbdbda67a28ec659dbd004dfa5153698d49
diversitydatakids.org <53530434+diversitydatakids@users.noreply.github.com>
2024-06-04 16:34:33 -0400

    Added overwrite option

--- R/SQL_write.R
+++ R/SQL_write.R
@@ -9,11 +9,12 @@
 #' @param table data table in R environment to write to SQL database
 #' @param table_id name the table that will appear in SQL database
 #' @param database name of database to write to in SQL database
+#' @param overwrite logical to overwrite table if it exists -- default is FALSE
 #'
 #' @param user username for SQL database, this function will only work with administrator credintials
 #' @param password password for SQL database administrator
 
-SQL_write <- function(table = NULL, table_id = NULL, database = NULL, user = NULL, password = NULL){
+SQL_write <- function(table = NULL, table_id = NULL, database = NULL, user = NULL, password = NULL, overwrite=F){
 
   # start time
   start <- Sys.time()
@@ -22,7 +23,13 @@ SQL_write <- function(table = NULL, table_id = NULL, database = NULL, user = NUL
   con <- RMariaDB::dbConnect(RMariaDB::MariaDB(),host='129.64.58.140',port=3306,user=user,password=password)
 
   # select database to write to
-  RMariaDB::dbGetQuery(con, paste0("USE ", database, ";"))
+  RMariaDB::dbExecute(con, paste0("USE ", database, ";"))
+
+  # check if table exists in database
+  check <- RMariaDB::dbGetQuery(con, paste0("SELECT count(*) FROM information_schema.tables WHERE table_schema = '",database, "' AND table_name = '", table_id, "' LIMIT 1;"))
+
+  # delete if overwrite == TRUE & check[[1]]
+  if(overwrite == TRUE & check[[1]]==1){RMariaDB::dbExecute(con, paste0("DROP TABLE ", table_id, ";"))}
 
   # write table
   dbWriteTable(con, table_id, table)
