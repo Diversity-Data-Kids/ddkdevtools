7fb633326e05b22d40d4916a309d4ef7ecb4e95a
briandevoe <bdevoe@brandeis.edu>
2024-07-30 16:58:46 -0400

    add kill connection function - bd

---
+++ R/SQL_kill_connection.R
@@ -0,0 +1,31 @@
+#' @name SQL_kill_connection
+#' @title SQL_kill_connection
+#' @author brian devoe
+#'
+#' @description
+#' kills processes from a database
+
+
+SQL_kill_connection <- function(user = NULL, password = NULL){
+
+  # connect to SQL server
+  con <- RMariaDB::dbConnect(RMariaDB::MariaDB(),host='129.64.58.140',port=3306,user=user,password=password)
+
+  # show process list
+  processList <- dbGetQuery(con, "SHOW PROCESSLIST;")
+
+  # subset process list to connections that are in "Sleep" state
+  processList <- processList[processList$Command == "Sleep",]
+
+  # loop through process list
+  for(id in processList$Id){
+
+    # kill all processes that are in "Sleep" state
+    RMariaDB::dbExecute(con, paste0("KILL ", id, ";"))
+
+  }
+
+  # disconnect from server
+  RMariaDB::dbDisconnect(con);rm(con)
+
+}
